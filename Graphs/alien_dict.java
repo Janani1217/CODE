bd9ee7f0867e47a8a529c435d47dab3b04e5123e
PN JANANI <59663462+Janani1217@users.noreply.github.com>
2023-11-18 20:52:59 +0530

    Update alien_dict.java

--- Graphs/alien_dict.java
+++ Graphs/alien_dict.java
@@ -1,72 +1,70 @@
-//{ Driver Code Starts
-/*package whatever //do not write package name here */
-
-import java.io.*;
-import java.util.*;
-import java.math.*;
-
 class Solution {
-    public void prepareAdjList(String[] dict, int n, List<List<Character>> adj) {
-        /* add the list for 26 char *****/
-        for (int i = 0; i < 26; i++) {
-            adj.add(new ArrayList<>());
-        }
+    public String alienOrder(String[] words) {
+        // using bfs + Topo sorting
+        int n = words.length;
 
-        for (int i = 0; i < n - 1; i++) {
-            String s1 = dict[i];
-            String s2 = dict[i + 1];
-
-            int j = 0;
-            int minLength = Math.min(s1.length(), s2.length());   /******* VVI */
-
-            while (j < minLength && s1.charAt(j) == s2.charAt(j)) {
-                j++;
-            }
-
-            if (j < s1.length() && j < s2.length()) {
-                char from = s1.charAt(j);
-                char to = s2.charAt(j);
-                adj.get(from - 'a').add(to);  /******* VVI */
-            }
-        }
-    }
-
-    public void dfsTopo(List<List<Character> > adj, Stack<Character> st, Map<Character, Integer> vis, char ch) {
-        if (vis.get(ch) == 1 || vis.get(ch) == 2) return;
-
-        vis.put(ch, 1);
-
-        for (char neigh : adj.get(ch - 'a')) {. /******* VVI */
-            if (vis.get(neigh) == 0) dfsTopo(adj, st, vis, neigh);
-        }
-
-        st.push(ch);
-        vis.put(ch, 2);
-    }
-
-    public String findOrder(String[] dict, int N, int K) {
-        List<List<Character>> adj = new ArrayList<>();
-        prepareAdjList(dict, N, adj);
-
-        Map<Character, Integer> vis = new HashMap<>();
-        for (char ch = 'a'; ch <= 'z'; ch++) {
-            vis.put(ch, 0);
-        }
-
-        Stack<Character> st = new Stack<>();
-        for (char ch = 'a'; ch <= 'z'; ch++) {
-            if (vis.get(ch) == 0) {
-                dfsTopo(adj, st, vis, ch);
+        Map<Character , List<Character>> adjlist = new HashMap<>();
+        Map<Character, Integer> counts = new HashMap<>();
+        
+        // creating adj list and init count of incoming to 0 for each char in the dict
+        for(String s : words){
+            for(Character ch : s.toCharArray()) {
+                counts.put(ch, 0);
+                adjlist.put(ch, new ArrayList<>());
             }
         }
 
-        StringBuilder res = new StringBuilder();
+        // prepare the adj list by comparing:
+        for(int i=0; i<n-1; i++) {
+            String w1 = words[i];
+            String w2 = words[i+1];
 
-        while (!st.isEmpty()) {
-            char c = st.pop();
-            res.append(c);
+            // check if w2 is prefix of w1 :
+            if(w1.length() > w2.length() && w1.startsWith(w2))
+                return ""; // since its not a valid ordering : w2 should come before
+
+            // traverse both w and check chars if not matching : process it
+            int minlen = Math.min(w1.length() , w2.length());
+
+            for(int j=0; j<minlen; j++) {
+                if(w1.charAt(j) != w2.charAt(j)) {
+                    // put in adj list
+                    adjlist.get(w1.charAt(j)).add(w2.charAt(j));
+                    // incr the count / incoming edge for secon char
+                    counts.put(w2.charAt(j), counts.get(w2.charAt(j))+1);
+                    break; // since u found the non-match char
+                }
+            }
         }
 
-        return res.toString();
+        // run bfs over the adjlist formed:
+        Queue<Character> q = new LinkedList<>();
+        StringBuilder sb = new StringBuilder();
+
+        // insert indegree of 0 into the queue first
+        for(Character ch : counts.keySet()) {
+            if(counts.get(ch).equals(0)) {
+                q.add(ch);
+            }
+        }
+
+        // run over queue:
+        while(!q.isEmpty()) {
+            Character ch = q.poll();
+            sb.append(ch);
+
+            for(Character neigh : adjlist.get(ch)) {
+                // decr the indegree
+                counts.put(neigh, counts.get(neigh)-1);
+                // check if indegree is 0:
+                if(counts.get(neigh).equals(0))
+                    q.add(neigh);
+            }
+        }
+
+        // check if any char is left unvisited:
+        if(counts.size() != sb.length())
+            return "";
+        return sb.toString();
     }
 }
